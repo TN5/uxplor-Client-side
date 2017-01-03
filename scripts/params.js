ded3ff44e750ea769d5d7b45d2d87af1b14b3526
Cassandra Torske <tors2720@gmail.com>
2017-01-03 14:23:48 -0700

    :pile of poo:

--- scripts/params.js
+++ scripts/params.js
@@ -7,4 +7,42 @@ $(document).ready(function(){
     })
     $('select').material_select();
   })
+
 })
+
+var longitude;
+var latitude;
+var type = 'bar';
+
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+function success(pos) {
+  var crd = pos.coords;
+
+  latitude = crd.latitude;
+  longitude = crd.longitude;
+
+  $('#param-submit').click(function() {
+
+    var radius = parseInt($('#radius').val()) * .3048;
+    var type = $('select.select').val();
+    if (type != '' && !isNaN(radius)){
+      var url = `http://localhost:3000/getlist?location=${latitude},${longitude}&radius=${radius}&type=${type}`;
+    } else {
+      throw new Error('invalid input');
+    }
+  })
+};
+
+function error(err) {
+  // console.warn(ERROR(`(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+setInterval(function (){
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}, 6000)
