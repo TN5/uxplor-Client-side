c93f12db9801a3b42cbdeaf041e9a70f904bbd21
Mike Dowd <mikedowd1972@gmail.com>
2017-01-03 15:06:15 -0700

    finished parampage

--- scripts/params.js
+++ scripts/params.js
@@ -27,11 +27,13 @@ function success(pos) {
   longitude = crd.longitude;
 
   $('#param-submit').click(function() {
-
     var radius = parseInt($('#radius').val()) * .3048;
     var type = $('select.select').val();
     if (type != '' && !isNaN(radius)){
       var url = `http://localhost:3000/getlist?location=${latitude},${longitude}&radius=${radius}&type=${type}`;
+      $.get(url, function(data) {
+        console.log(data);
+      });
     } else {
       alert("Invalid Input");
     }
@@ -48,16 +50,36 @@ setInterval(function (){
 }, 6000)
 
 $('#walk-btn').click(function() {
-  $('#radius').val('1000ft');
+  $('#radius').val('1000');
   $('#radius-label').hide();
 });
 
 $('#bike-btn').click(function() {
-  $('#radius').val('6000ft');
+  $('#radius').val('6000');
   $('#radius-label').hide();
 });
 
 $('#car-btn').click(function() {
-  $('#radius').val('12000ft');
+  $('#radius').val('12000');
   $('#radius-label').hide();
 });
+
+$('#incrementer').click(function() {
+  changeRadius(1);
+});
+
+$('#decrementer').click(function() {
+  changeRadius(-1);
+});
+
+function changeRadius(coef) {
+  var change = 100;
+  var radius = parseInt($('#radius').val());
+  if(radius >= 6000) {
+    change = 1000;
+  } else if(radius >= 2000) {
+    change = 200;
+  }
+  radius += (change * coef);
+  $('#radius').val(`${radius}ft`);
+}
