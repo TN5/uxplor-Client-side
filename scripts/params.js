b137d170ab8202c67039adfad1c600201167e285
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-01-03 12:53:57 -0700

    create text type file and populate dropdown

--- scripts/params.js
+++ scripts/params.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $('select').material_select();
+  $.get('types.txt', function(data) {
+    console.log(data);
+    data = data.split('\n');
+    data.forEach(function(i) {
+      console.log(`<option class="option">${i}</option>`);
+      $('select.select').append(`<option class="option">${i}</option>`)
+    })
+    $('select').material_select();
+  })
 })
