6108c6dc9d389296dc53b2ad71a9e30615d23ec4
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-01-03 13:16:21 -0700

    prep to merge

--- scripts/params.js
+++ scripts/params.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
   $('select').material_select();
   $.get('types.txt', function(data) {
-    console.log(data);
     data = data.split('\n');
     data.forEach(function(i) {
-      console.log(`<option class="option">${i}</option>`);
       $('select.select').append(`<option class="option">${i}</option>`)
     })
     $('select').material_select();
