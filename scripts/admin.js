958f3cb51201296c54aac4a0b9e9ff1622524b5f
ZacJocelyn <zacjocelyn@gmail.com>
2017-01-06 13:36:04 -0700

    got the put request

--- scripts/admin.js
+++ scripts/admin.js
@@ -10,31 +10,53 @@ $(document).ready( function() {
 });
 
 $('#flagged').click(function() {
-  $.get('http://localhost:3000/flag', function(flagged) {
+  $.get('http://uxplor.herokuapp.com/flag', function(flagged) {
     var $list = $('#flags')
     $('#ratings').html('');
     $list.html('');
     for (var i = 0; i < flagged.length; i++) {
-      var name = flagged[i].name;
-      var googleId = flagged[i].google_id;
-      var type = flagged[i].flag_type;
-      var userId = flagged[i].user_id;
-      $list.append(`
-        <li class="collection-item">
-          <span class="title">Name: ${name}</span>
-          <p>Google ID: <span class="googleid">${googleId}<span> <br>
-             Type: <span class="type">${type}<span> <br>
-             User ID: <span class="userid">${userId}<span>
-          </p>
-        </li>
+      if (flagged[i].flagged === true) {
+        var name = flagged[i].name;
+        var googleId = flagged[i].google_id;
+        var type = flagged[i].flag_type;
+        var userId = flagged[i].user_id;
+        $list.append(`
+          <li class="collection-item">
+            <span class="title">Name: ${name}</span>
+            <p>Google ID: <span class="googleid">${googleId}<span> <br>
+               Type: <span class="type">${type}<span> <br>
+               Flag ID: <span class="flagid">${flagged[i].id}<span><br>
+               User ID: <span class="userid">${userId}<span>
+            </p>
+            <a id="unflag${flagged[i].id}" class="waves-effect waves-light btn">Unflag</a>
+          </li>
 
-        `)
+          `);
+        var obj = {
+          name: name,
+          google_id: googleId,
+          flagged: false,
+          flag_type: type,
+          user_id: userId
+        }
+        var flags = flagged[i].id
+        $($('#unflag' + flagged[i].id)[0]).click(function() {
+          $.ajax({
+            url: `https://uxplor.heroku.com/flag/${flags}`,
+            method: 'PUT',
+            data: obj,
+            success: function(response) {
+              window.location.reload();
+            }
+          });
+        });
+      }
     };
   });
 });
 
 $('#rating').click(function() {
-  $.get('http://localhost:3000/feedback', function(rating) {
+  $.get('https://uxplor.herokuapp.com/feedback', function(rating) {
     var $ratings = $('#ratings')
     $('#flags').html('');
     $ratings.html('');
