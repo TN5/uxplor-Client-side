4f3e789e8acb6245d3cc0f279612be4b0089039a
matt-winzer <matt.winzer@gmail.com>
2017-01-08 23:33:41 -0700

    :sparkles: made badges display better on all view sizes

--- scripts/user_badge.js
+++ scripts/user_badge.js
@@ -5,13 +5,13 @@ $(document).ready( function() {
     closeOnClick: true,
     draggable: true
   });
-  
+
   var id = 1;
   $.get('badges.txt', function(data) {
     let dataArray = data.split('\n')
     dataArray.pop()
     dataArray.forEach(function(i) {
-       $('#badge-container').append(  `<div id="image-holder" class="image-holder col s2">
+       $('#badge-container').append(  `<div id="image-holder" class="image-holder col s6 m4 l3">
           <img class="image-size" id="badge-image" src="${i}">
          </div>`)
     })
